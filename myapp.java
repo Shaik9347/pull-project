0c6e5af71d0d53daa6dc523ead0f74b5da9e127a
modhi <modhi@gamil.com>
2025-08-03 11:48:17 +0000

    upadtedbymodhi

--- myapp.java
+++ myapp.java
@@ -3,6 +3,8 @@ public class HelloWorld
 	public static void main(String[] args) {
 		System.out.println("I am java lerner ")
 		sytem.out.println("I am wirting my first progarm")
+		System.out.println("git pull")
+		System.out.prontln("git push")
 		System.out.println("Hello World!");
 	}
 }
